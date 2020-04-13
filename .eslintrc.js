996583acc9e882f4c09aaeaa19b0bf74e346c6fe
Niimi Youki <niimiyouki@gmail.com>
2020-04-13 12:32:47 +0900

    設定を少し変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    semi: [2, 'never'],
+    'comma-dangle': ['error', 'never'],
+    eqeqeq: ['error', 'smart'],
+    'no-param-reassign': ['error', { props: false }]
+  }
 };
--- App.js
+++ App.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from 'react'
+import {StyleSheet, Text, View} from 'react-native'
 
 export default function HelloWorldApp() {
   return (
-    <View style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}>
+    <View style={styles.container}>
       <Text>Hello, world!!!</Text>
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
