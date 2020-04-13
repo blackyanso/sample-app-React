f071b1f0061d31837c5ec22dfb8ae84dd9a39b87
Niimi Youki <niimiyouki@gmail.com>
2020-04-13 15:12:56 +0900

    土台ページ作成

--- App.js
+++ App.js
@@ -1,18 +1,48 @@
 import React from 'react'
 import {StyleSheet, Text, View} from 'react-native'
+import {NativeRouter, Route, Link} from 'react-router-native'
+
+import TopCamera from './components/pages/TopCamera/index'
+import ChooseFilter from './components/pages/ChooseFilter/index'
+import Gallery from './components/pages/Gallery/index'
+import GalleryPreview from './components/pages/GalleryPreview/index'
 
 export default function HelloWorldApp() {
   return (
-    <View style={styles.container}>
-      <Text>Hello, world!!!</Text>
-    </View>
+    <NativeRouter>
+      <View style={styles.debug}>
+        <Link to="/">
+          <Text>TopCamera</Text>
+        </Link>
+        <Link to="/ChooseFilter">
+          <Text>ChooseFilter</Text>
+        </Link>
+        <Link to="/Gallery">
+          <Text>Gallery</Text>
+        </Link>
+        <Link to="/GalleryPreview">
+          <Text>GalleryPreview</Text>
+        </Link>
+      </View>
+      <View style={styles.container}>
+        <Route exact path="/" component={TopCamera} />
+        <Route path="/ChooseFilter" component={ChooseFilter} />
+        <Route path="/Gallery" component={Gallery} />
+        <Route path="/GalleryPreview" component={GalleryPreview} />
+      </View>
+    </NativeRouter>
   )
 }
 
 const styles = StyleSheet.create({
+  debug: {
+    display: 'flex',
+    backgroundColor: '#f0f0f0'
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    padding: 20,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start'
   }
 })
---
+++ components/pages/GalleryPreview/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+
+export default function GalleryPreview() {
+  return (
+    <View>
+      <Text>GalleryPreview</Text>
+    </View>
+  )
+}
