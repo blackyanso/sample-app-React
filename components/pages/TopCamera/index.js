73057f66cd323861048d74f5564c885d255b8f5c
Masanao Imai <masa7351@gmail.com>
2020-04-14 14:20:03 +0900

    iPhone実機でカメラ撮影出来るように修正

--- components/pages/TopCamera/index.js
+++ components/pages/TopCamera/index.js
@@ -1,48 +1,80 @@
-import React from 'react'
-import {Text, View, StyleSheet, Button} from 'react-native'
+import React, {useRef} from 'react'
+import {Button, SafeAreaView, StatusBar, StyleSheet, View} from 'react-native'
 import {RNCamera} from 'react-native-camera'
+import {Colors} from 'react-native/Libraries/NewAppScreen'
 
-export default function TopCamera() {
-  async function shotPictire(a) {
+const TopCamera = () => {
+  const cameraRef = useRef(null)
+
+  const takePicture = async () => {
+    // https://qiita.com/hazigin/items/900d196fef0d85570ca8
+    // https://github.com/react-native-community/react-native-camera/blob/master/docs/RNCamera.md
+    // https://qiita.com/arakappa/items/edd70ab9102a8566c145
     const options = {
       quality: 0.95,
       base64: true,
       width: 320,
       height: 320
     }
-    const shotData = await this.camera.takePictureAsync(options)
-    console.log(shotData.base64)
+    if (cameraRef && cameraRef.current) {
+      const shotData = await cameraRef.current.takePictureAsync(options)
+      console.log('base64 log:', shotData.base64)
+    }
   }
 
   return (
-    <View>
-      <Text>TopCamera</Text>
-      <RNCamera
-        ref={ref => {
-          this.camera = ref
-        }}
-        style={styles.preview}
-        type={RNCamera.Constants.Type.back}
-        flashMode={RNCamera.Constants.FlashMode.on}
-        androidCameraPermissionOptions={{
-          title: 'Permission to use camera',
-          message: 'We need your permission to use your camera',
-          buttonPositive: 'Ok',
-          buttonNegative: 'Cancel'
-        }}
-        onGoogleVisionBarcodesDetected={({barcodes}) => {
-          console.log(barcodes)
-        }}
-        ratio="1:1"
-      />
-      <Button title="撮影" onPress={shotPictire.bind(this)} />
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <View>
+          <View style={styles.cameraContainer}>
+            <RNCamera
+              ref={cameraRef}
+              style={styles.cameraPreview}
+              type={RNCamera.Constants.Type.back}
+              flashMode={RNCamera.Constants.FlashMode.on}
+              androidCameraPermissionOptions={{
+                title: 'Permission to use camera',
+                message: 'We need your permission to use your camera',
+                buttonPositive: 'Ok',
+                buttonNegative: 'Cancel'
+              }}
+              onGoogleVisionBarcodesDetected={({barcodes}) => {
+                console.log(barcodes)
+              }}
+              ratio="1:1"
+            />
+            <Button onPress={takePicture} title="撮影" />
+          </View>
+        </View>
+      </SafeAreaView>
+    </>
   )
 }
 
 const styles = StyleSheet.create({
-  preview: {
-    width: 320,
-    height: 320
+  body: {
+    backgroundColor: Colors.white
+  },
+  padding: {
+    padding: 10
+  },
+  waringText: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: 'red'
+  },
+  cameraContainer: {
+    zIndex: 0,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cameraPreview: {
+    zIndex: 0,
+    width: '100%',
+    aspectRatio: 1 // Androidではここでのプレビュー画面の比率の指定が効かない...
   }
 })
+
+export default TopCamera
