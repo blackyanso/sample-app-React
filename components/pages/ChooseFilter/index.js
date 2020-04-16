cfb91f5f82e0db14802b42ad8c6c43d1f7589ffc
Niimi Youki <niimiyouki@gmail.com>
2020-04-16 11:43:38 +0900

    webview表示

--- components/pages/ChooseFilter/index.js
+++ components/pages/ChooseFilter/index.js
@@ -1,10 +1,34 @@
+import 'react-native-get-random-values'
 import React from 'react'
-import {Text, View} from 'react-native'
+import {StyleSheet, Text, View} from 'react-native'
+import {WebView} from 'react-native-webview'
+const html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body></body>
+</html>
+`
 
 export default function ChooseFilter() {
   return (
     <View>
       <Text>ChooseFilter</Text>
+      <View style={styles.wrap}>
+        <WebView scalesPageToFit={false} source={{html}} />
+      </View>
+      <Text>ChooseFilter</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  wrap: {
+    width: 0,
+    height: 0
+  }
+})
