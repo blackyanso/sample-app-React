6d04a11e81a5ae211a0589691d6f3db5027c2f8c
Niimi Youki <niimiyouki@gmail.com>
2020-04-17 14:55:42 +0900

    webviewへ情報を渡してフィルターをかけたのをネイティブで受け取って画像表示

--- components/pages/ChooseFilter/index.js
+++ components/pages/ChooseFilter/index.js
@@ -1,39 +1,72 @@
 import 'react-native-get-random-values'
-import React from 'react'
-import {StyleSheet, Text, View, ScrollView} from 'react-native'
+import React, {useState} from 'react'
+import {StyleSheet, Switch, Text, View, Image, ScrollView} from 'react-native'
 import {WebView} from 'react-native-webview'
-const html = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body></body>
-</html>
-`
 
-export default function ChooseFilter({match}) {
+import sample64 from './sample64'
+
+export default function({match}) {
+  const [imageData, setImageData] = useState()
+
+  const styles = StyleSheet.create({
+    wrap: {
+      width: '100%',
+      height: 50
+    },
+    image: {
+      width: 300,
+      height: 300
+    }
+  })
+
+  const html = /*html*/ `
+    <body>aaa</body>
+    <script src="https://cdn.rawgit.com/girliemac/filterous-2/1fc15582/demo-browser/filterous2.min.js"></script>
+    <script>
+      const baseData = '${sample64}'
+
+      function filter(filterStyle) {
+      const resultImage = new Image()
+      const baseImage = new Image()
+      baseImage.src = baseData
+
+      resultImage.onload = () => {
+      window.ReactNativeWebView.postMessage(resultImage.src)
+      }
+
+      filterous
+        .importImage(baseImage)
+        .applyInstaFilter(filterStyle)
+        .renderHtml(resultImage)
+      }
+
+      filter('1977')
+    </script>
+  `
+  function decodeBase64(base64) {
+    return base64.replace(/-/g, '+').replace(/_/g, '/')
+  }
+
+  function filtered(event) {
+    const {data} = event.nativeEvent
+    setImageData(data)
+  }
+
   return (
     <ScrollView>
+      <Switch />
+      <Image style={styles.image} source={{uri: imageData}} />
       <Text>ChooseFilter</Text>
       <View style={styles.wrap}>
-        <WebView scalesPageToFit={false} source={{html}} />
+        <WebView
+          scalesPageToFit={false}
+          source={{html}}
+          onMessage={filtered}
+          mixedContentMode={'always'}
+        />
       </View>
       <Text>ChooseFilter</Text>
       <Text>Base64: {decodeBase64(match.params.base64)}</Text>
     </ScrollView>
   )
 }
-
-const decodeBase64 = base64 => {
-  return base64.replace(/-/g, '+').replace(/_/g, '/')
-}
-
-const styles = StyleSheet.create({
-  wrap: {
-    width: 0,
-    height: 0
-  }
-})
