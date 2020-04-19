24f5b97d9027f52d4c2a67c9f820b4cc837ba03b
Yoshima Fukuda <yoshima.fukuda@codeberry.co.jp>
2020-04-20 08:29:46 +0900

    init

--- components/pages/TopCamera/index.js
+++ components/pages/TopCamera/index.js
@@ -1,10 +1,19 @@
-import React, {useRef} from 'react'
-import {Button, SafeAreaView, StatusBar, StyleSheet, View} from 'react-native'
+import React, {useState, useRef} from 'react'
+import {
+  Button,
+  Platform,
+  NativeModules,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  View
+} from 'react-native'
 import {RNCamera} from 'react-native-camera'
 import {Colors} from 'react-native/Libraries/NewAppScreen'
 
 const TopCamera = () => {
   const cameraRef = useRef(null)
+  const [isRecording, toggleRecording] = useState(false)
 
   const takePicture = async () => {
     // https://qiita.com/hazigin/items/900d196fef0d85570ca8
@@ -18,9 +27,54 @@ const TopCamera = () => {
     }
     if (cameraRef && cameraRef.current) {
       const shotData = await cameraRef.current.takePictureAsync(options)
+      console.log('uri:', shotData.uri)
       console.log('base64 log:', shotData.base64)
     }
   }
+
+  const takeVideo = async () => {
+    const options = {
+      mute: false,
+      maxDuration: 5,
+      quality: RNCamera.Constants.VideoQuality['288p']
+    }
+
+    if (cameraRef && cameraRef.current && !isRecording) {
+      try {
+        const promise = cameraRef && cameraRef.current.recordAsync(options)
+
+        if (promise) {
+          console.log('start takeVideo')
+          toggleRecording(true)
+          const data = await promise
+          // console.log('takeVideo', data)
+          // await convertToMp4(data.uri)
+          toggleRecording(false)
+        }
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  }
+
+  const stopVideo = async () => {
+    console.log('stop takeVideo')
+    await cameraRef.current.stopRecording()
+  }
+
+  // const convertToMp4 = async (path: string) => {
+  //   console.log('convert from .mov to .mp4')
+  //   console.log('path:', path)
+  //   if (Platform.OS === 'ios') {
+  //     const result = await NativeModules.ConvertToMp4.convert(path).catch(
+  //       (err: any) => {
+  //         console.log(err)
+  //       }
+  //     )
+  //
+  //     console.log(result)
+  //   }
+  // }
 
   return (
     <View>
@@ -40,9 +94,26 @@ const TopCamera = () => {
         }}
         ratio="1:1"
       />
-      <Button onPress={takePicture} title="撮影" />
+      <Button onPress={takePicture} title="写真撮影" />
+      {isRecording ? (
+        <RenderStopRecBtn action={stopVideo} />
+      ) : (
+        <RenderRecBtn action={takeVideo} />
+      )}
     </View>
   )
+}
+
+type buttonAction = () => void
+interface RecordProps {
+  action: buttonAction;
+}
+const RenderRecBtn: React.SFC<RecordProps> = ({action}) => {
+  return <Button onPress={action} title="動画撮影開始" />
+}
+
+const RenderStopRecBtn: React.SFC<RecordProps> = ({action}) => {
+  return <Button onPress={action} title="動画撮影終了" />
 }
 
 const styles = StyleSheet.create({
