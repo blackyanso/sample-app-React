962edd2aa44c68d4061e73e163b9184dba00000a
Ayano KAITO <ayano.kaito@codeberry.co.jp>
2020-04-20 16:13:27 +0900

    プレビュー表示

--- components/pages/GalleryPreview/index.js
+++ components/pages/GalleryPreview/index.js
@@ -1,13 +1,28 @@
 import React from 'react'
-import {Text, View} from 'react-native'
+import {Text, View, Image, StyleSheet} from 'react-native'
+import {Link} from 'react-router-native'
 
 export default function GalleryPreview(props) {
+  const styles = StyleSheet.create({
+    imagePreview: {
+      width: 330,
+      height: 330
+    },
+    back: {
+      color: '#ADD8E6',
+      fontSize: 20,
+      margin: 5
+    }
+  })
   const imageUrl = props.location.state.url
   console.log(imageUrl)
 
   return (
     <View>
-      <Text>{imageUrl}</Text>
+      <Link to={{pathname: '/Gallery'}}>
+        <Text style={styles.back}>◀</Text>
+      </Link>
+      <Image style={styles.imagePreview} source={{uri: imageUrl}} />
     </View>
   )
 }
