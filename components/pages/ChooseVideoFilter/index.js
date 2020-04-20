eb7e771bf27c5d7a395778db9befbd30d2f8b40d
Masanao Imai <masa7351@gmail.com>
2020-04-20 16:38:46 +0900

    レビュー指摘を対応

--- components/pages/ChooseVideoFilter/index.js
+++ components/pages/ChooseVideoFilter/index.js
@@ -2,19 +2,20 @@ import React from 'react'
 import {Text, View, Button} from 'react-native'
 import {RNFFmpeg} from 'react-native-ffmpeg'
 
-const ChooseVideoFilter = ({match}) => {
+export default function ChooseVideoFilter({match}) {
   const inputFile = decodeURIComponent(match.params.filePath).replace(
     /file:\/\//g,
     ''
   )
-  const onFilter = async () => {
-    const outputFile = inputFile.replace('.mov', '.mp4')
+  async function onFilter() {
+    // Androidはデフォルト撮影したものが.mp4で同名では上書きができないことを考慮してファイル名を変更
+    let outputFile = inputFile.replace('.mov', '.mp4')
+    outputFile = outputFile.replace('.mp4', '.filter.mp4')
     // モノクロフィルター加工
     // https://video.stackexchange.com/questions/27744/how-to-monochrome-haze-video-effect-in-ffmpeg
-    const executeCommand = `-i ${inputFile} -vf hue=s=0 -pix_fmt yuv420p ${outputFile}`
-    await RNFFmpeg.execute(executeCommand).then((result) =>
-      console.log('FFmpeg process exited with rc ' + result.rc)
-    )
+    const executeCommand = `-i ${inputFile} -y -vf hue=s=0 -pix_fmt yuv420p ${outputFile}`
+    const result = await RNFFmpeg.execute(executeCommand)
+    console.log('FFmpeg process exited with rc ' + result.rc)
   }
 
   return (
@@ -25,5 +26,3 @@ const ChooseVideoFilter = ({match}) => {
     </View>
   )
 }
-
-export default ChooseVideoFilter
